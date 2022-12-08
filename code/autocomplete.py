e9f71ac2dd8a106c2416b4d9f1d05db8494534c4
Rémy <25103686+RemyAtOVH@users.noreply.github.com>
2022-12-08 20:20:44 +0000

    Added new command

--- code/autocomplete.py
+++ code/autocomplete.py
@@ -42,3 +42,34 @@ async def get_project_list(ctx: discord.AutocompleteContext):
                 )
 
         return project_list
+
+async def get_user_list(ctx: discord.AutocompleteContext):
+    """Function to build and serve an Autocomplete list of Users."""
+    try:
+        ovh_client = ovh.Client(
+            endpoint=OVH_ENDPOINT,
+            application_key=OVH_AK,
+            application_secret=OVH_AS,
+            consumer_key=OVH_CK,
+            )
+
+        users = ovh_client.get(
+            f'/cloud/project/{ctx.options["projectid"]}/user'
+            )
+    except Exception as e:
+        logger.error(f'Autocomplete generation KO [{e}]')
+        return []
+    else:
+        if users is None or len(users) == 0:
+            return []
+
+        user_list = []
+        for user in users:
+            user_list.append(
+                discord.OptionChoice(
+                    f"{user['username']} ({user['description']})",
+                    value=f"{user['id']}",
+                    )
+                )
+
+        return user_list
--- code/imabot.py
+++ code/imabot.py
@@ -24,6 +24,7 @@ from variables import (
 
 from autocomplete import (
     get_project_list,
+    get_user_list,
 )
 
 # Log Internal imports
@@ -206,6 +207,233 @@ async def project(
                     ),
                 colour=discord.Colour.green()
             )
+            await ctx.respond(embed=embed)
+
+        logger.debug(f'[#{channel}][{name}] └──> Queries OK')
+        return
+
+@group_singouin.command(
+    description='/ovh Commands related to OpenStack users',
+    default_permission=False,
+    name='user',
+    )
+@option(
+    "action",
+    description="Users action",
+    autocomplete=discord.utils.basic_autocomplete(
+        [
+            discord.OptionChoice("delete", value="delete"),
+            discord.OptionChoice("list", value="list"),
+            discord.OptionChoice("show", value="show"),
+            ]
+        )
+    )
+@option(
+    "projectid",
+    description="Project ID",
+    autocomplete=get_project_list,
+    required=False,
+    )
+@option(
+    "userid",
+    description="User ID",
+    autocomplete=get_user_list,
+    required=False,
+    )
+async def user(
+    ctx,
+    action: str,
+    projectid: str,
+    userid: str,
+):
+    """
+    This part performs actions on Public Cloud (OpenStack) users
+    So far:
+    - delete: Deletes a specific User
+    - list: Displays the list of ALL Users
+    - show: Displays the details of a specific User
+    """
+    # As we rely on potentially a lot of API calls, we need time to answer
+    await ctx.defer()
+    # Pre-flight checks
+    if ctx.channel.type is discord.ChannelType.private:
+        channel = ctx.channel.type
+    else:
+        channel = ctx.channel.name
+    name = ctx.author.name
+    logger.info(
+        f'[#{channel}][{name}] /ovh user '
+        f'{action} {projectid} {userid}'
+        )
+
+    if action == 'list':
+        # We start with the headers
+        embed_field_value_table = {
+            'User Name': [],
+            'User Desc.': [],
+        }
+
+        try:
+            projects = ovh_client.get('/cloud/project')
+        except Exception as e:
+            msg = f'API calls KO [{e}]'
+            logger.error(msg)
+            embed = discord.Embed(
+                description=msg,
+                colour=discord.Colour.red()
+            )
+            await ctx.respond(embed=embed)
+            return
+
+        # We loop over the projects
+        try:
+
+            embed = discord.Embed(
+                title=f'**{my_nic}**',
+                colour=discord.Colour.green()
+                )
+
+            for project_id in projects:
+                project = ovh_client.get(f'/cloud/project/{project_id}')
+
+                if project['status'] == 'suspended':
+                    # Suspended Projects cannot be queried later
+                    embed.add_field(
+                        name=f'Project ID: **{project_id}**',
+                        value='(Suspended)',
+                        inline=False,
+                        )
+                    continue
+
+                users = ovh_client.get(f'/cloud/project/{project_id}/user')
+                if len(users) == 0:
+                    # There is no Users in the Project
+                    embed.add_field(
+                        name=f'Project ID: **{project_id}**',
+                        value='No Users',
+                        inline=False,
+                        )
+                    continue
+
+                for user in users:
+                    # We loop over the users to grab their names
+                    embed_field_value_table['User Name'].append(user['username'])
+                    embed_field_value_table['User Desc.'].append(user['description'])
+
+                embed.add_field(
+                    name=f'Project ID: **{project_id}**',
+                    value=(
+                        '```' +
+                        tabulate(
+                            embed_field_value_table,
+                            headers='keys',
+                            tablefmt='pretty',
+                            stralign='right',
+                            ) +
+                        '```'
+                        ),
+                    inline=False,
+                    )
+
+                await ctx.interaction.edit_original_response(
+                    embed=embed
+                    )
+        except Exception as e:
+            msg = f'API calls KO [{e}]'
+            logger.error(msg)
+            embed = discord.Embed(
+                description=msg,
+                colour=discord.Colour.red()
+            )
+            await ctx.respond(embed=embed)
+            return
+        else:
+            logger.debug(f'[#{channel}][{name}] └──> Queries OK')
+            return
+    elif action == 'show':
+        if projectid is None or userid is None:
+            logger.error('Missing mandatory option(s)')
+            msg = (
+                'Check that you provided all variables: \n'
+                ' - `projectid` \n'
+                ' - `userid` \n'
+                )
+            embed = discord.Embed(
+                description=msg,
+                colour=discord.Colour.red()
+            )
+            await ctx.respond(embed=embed)
+            return
+
+        try:
+            user = ovh_client.get(
+                f'/cloud/project/{projectid}/user/{userid}'
+                )
+        except Exception as e:
+            msg = f'API calls KO [{e}]'
+            logger.error(msg)
+            embed = discord.Embed(
+                description=msg,
+                colour=discord.Colour.red()
+            )
+            await ctx.respond(embed=embed)
+            return
+        else:
+            embed = discord.Embed(
+                description=(
+                    f'```json\n'
+                    f'{json.dumps(user, indent=4)}'
+                    f'\n```'
+                    ),
+                colour=discord.Colour.green()
+            )
+            await ctx.respond(embed=embed)
+
+        logger.debug(f'[#{channel}][{name}] └──> Queries OK')
+        return
+    elif action == 'delete':
+        if projectid is None or userid is None:
+            logger.error('Missing mandatory option(s)')
+            msg = (
+                'Check that you provided all variables: \n'
+                ' - `projectid` \n'
+                ' - `userid` \n'
+                )
+            embed = discord.Embed(
+                description=msg,
+                colour=discord.Colour.red()
+            )
+            await ctx.respond(embed=embed)
+            return
+
+        try:
+            user = ovh_client.get(
+                f'/cloud/project/{projectid}/user/{userid}'
+                )
+            ovh_client.delete(
+                f'/cloud/project/{projectid}/user/{userid}'
+                )
+        except Exception as e:
+            msg = f'API calls KO [{e}]'
+            logger.error(msg)
+            embed = discord.Embed(
+                description=msg,
+                colour=discord.Colour.red()
+            )
+            await ctx.respond(embed=embed)
+            return
+        else:
+            embed = discord.Embed(
+                title=f'**{my_nic}**',
+                description=(
+                    f"The Public Cloud (OpenStack) User "
+                    f"`{user['username']}` ({user['description']}) "
+                    f"was deleted"
+                    ),
+                colour=discord.Colour.green()
+            )
+            embed.set_footer(text=f"Project ID: {projectid}")
+
             await ctx.respond(embed=embed)
 
         logger.debug(f'[#{channel}][{name}] └──> Queries OK')
